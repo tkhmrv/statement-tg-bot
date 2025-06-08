940bf3999f2063e9b3e824ddf2e96cbf535596e2
Semyon <semyon.tkhmrv@gmail.com>
2025-06-08 21:25:13 +0300

    feat: enhance ping server to use environment variable for port configuration

--- ping_server.py
+++ ping_server.py
@@ -1,17 +1,16 @@
 from flask import Flask
+from threading import Thread
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Bot is alive!"
-
 @app.route("/ping")
 def ping():
-    return "pong"
+    return "pong", 200
 
 def start_ping_server():
-    from threading import Thread
     def run():
-        app.run(host="0.0.0.0", port=3000)
+        port = int(os.environ.get("PORT", 3000))
+        app.run(host="0.0.0.0", port=port)
     Thread(target=run).start()
